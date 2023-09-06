89c3c14913b511a9810008a3bb7e387dae82ad5b
seonwooke <seonookkim@gmail.com>
2023-09-06 15:05:39 +0900

    :tada: FCM Study Begin!
    FCM Study Begin!

---
+++ lib/views/init/init_view.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+class InitPage extends StatelessWidget {
+  const InitPage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      color: Colors.white,
+      child: const Center(
+        child: Text(
+          'Init View',
+        ),
+      ),
+    );
+  }
+}
